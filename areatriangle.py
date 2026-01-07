6835bcee8c17ea7cded95e742660aea60fe8f298
Gouri <gourimore512@gmail.com>
2026-01-07 14:24:54 +0530

    python simple program

---
+++ areatriangle.py
@@ -0,0 +1,3 @@
+h = float(input("Enter the height of the triangle: "))
+b = float(input("Enter the base of the triangle: "))
+print("The area of the triangle is:", 0.5 * b * h)
---
+++ input.py
@@ -0,0 +1,13 @@
+a = 20 
+b = 10.5
+c = "Hello"
+d = 'a'
+print (a, type (a))
+print (b, type (b))
+print (c, type (c))
+print (d, type (d))
+
+a = int (input ("Enter an integer: "))
+b = float (input ("Enter a float: "))
+c = input ("Enter a string: ")
+d = input ("Enter a character: ")
