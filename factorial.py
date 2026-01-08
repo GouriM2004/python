b302b7385bc6e27d3629404c4aaad8bd284dbbbd
Gouri <gourimore512@gmail.com>
2026-01-08 13:54:46 +0530

    simple factorial program using python

--- factorial.py
+++ factorial.py
@@ -1,10 +1,14 @@
+num = int(input("Enter a number for factorial calculation: "))
+
 def factorial(n):
-   
     if n < 0:
-        raise ValueError("Factorial is not defined for negative numbers")
-    if n == 0 or n == 1:
+        return "Factorial is not defined for negative numbers."
+    elif n == 0 or n == 1:
         return 1
-    return n * factorial(n - 1)
+    else:
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
 
-
-
+print(f"The factorial of {num} is: {factorial(num)}")
