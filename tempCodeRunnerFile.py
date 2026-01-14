ca85e277027df6c49c201c14813c1e34029d33eb
GouriM2004 <gourimore512@gmail.com>
2026-01-14 15:48:33 +0000

    fixed the tempCodeRunnerFile

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,8 @@
 
+a = int(input("Enter 1st number"))
+b = int(input("Enter 2nd number"))
+print(f"Before swap: a = {a}, b = {b}")
+temp = a
 a = b
 b = temp
-
 print(f"After swap: a = {a}, b = {b}")
