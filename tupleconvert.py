5ef4f2af173be8a261950e0bf9b2735b046bd0a1
Gouri <gourimore512@gmail.com>
2026-01-23 20:03:51 +0530

    Convert tuple to list and vice versa

---
+++ tupleconvert.py
@@ -0,0 +1,15 @@
+# Program to convert tuple to list and list to tuple
+
+# Tuple to List
+my_tuple = (10, 20, 30, 40)
+print("Original Tuple:", my_tuple)
+
+my_list = list(my_tuple)
+print("Converted List:", my_list)
+
+# List to Tuple
+new_list = [50, 60, 70, 80]
+print("Original List:", new_list)
+
+new_tuple = tuple(new_list)
+print("Converted Tuple:", new_tuple)
