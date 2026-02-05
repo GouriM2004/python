1606875d46e637bfadd22a61e1d1421f6ac1e6ee
GouriM2004 <gourimore512@gmail.com>
2026-02-05 15:30:57 +0000

    created a program to check if the number is palindrome or not

---
+++ Palindrome.py
@@ -0,0 +1,12 @@
+n = int(input("Enter number: "))
+temp = n
+rev = 0
+
+while temp > 0:
+    rev = rev * 10 + temp % 10
+    temp //= 10
+
+if rev == n:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
