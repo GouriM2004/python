a654b54ffd6cf0dbffaf3ef092235ed2f8be7358
Gouri <gourimore512@gmail.com>
2026-02-10 10:46:54 +0530

    finding some error

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -10,14 +10,3 @@ while True:
     try:
         guess = int(input("Enter your guess: "))
         attempts += 1
-
-        if guess < secret_number:
-            print("Too Low ⬇️")
-        elif guess > secret_number:
-            print("Too High ⬆️")
-        else:
-            print(f"🎉 Correct! You guessed the number in {attempts} attempts.")
-            break
-
-    except ValueError:
-        print("Please enter a valid number!")
