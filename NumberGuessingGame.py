0e604adabc33be6c4c177598630d4c58f34a4094
Gouri <gourimore512@gmail.com>
2026-02-10 10:48:54 +0530

    finding some other error also

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -7,6 +7,14 @@ print("🎯 Welcome to Number Guessing Game!")
 print("Guess the number between 1 and 100")
 
 while True:
-    try:
-        guess = int(input("Enter your guess: "))
-        attempts += 1
+   
+        if guess < secret_number:
+            print("Too Low ⬇️")
+        elif guess > secret_number:
+            print("Too High ⬆️")
+        else:
+            print(f"🎉 Correct! You guessed the number in {attempts} attempts.")
+            break
+
+    except ValueError:
+        print("Please enter a valid number!")
