b67e197f12a92015a62770811a2705c98da92490
GouriM2004 <gourimore512@gmail.com>
2026-02-27 12:10:10 +0000

    all program is done

--- commonelement.py
+++ commonelement.py
@@ -7,6 +7,9 @@ n1 = int(input("Enter number of elements in first list: "))
 for i in range(n1):
     list1.append(int(input(f"Enter element {i+1}: ")))
 
+n2 = int(input("Enter number of elements in second list: "))
+for i in range(n2):
+    list2.append(int(input(f"Enter element {i+1}: ")))
 
 common = []
 
