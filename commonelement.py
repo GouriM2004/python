57a1a91b998d0f70398021f200960aac0f9c6642
GouriM2004 <gourimore512@gmail.com>
2026-02-27 12:06:57 +0000

    adding common elements function

--- commonelement.py
+++ commonelement.py
@@ -11,4 +11,10 @@ n2 = int(input("Enter number of elements in second list: "))
 for i in range(n2):
     list2.append(int(input(f"Enter element {i+1}: ")))
 
+common = []
 
+for item in list1:
+    if item in list2 and item not in common:
+        common.append(item)
+
+print("Common elements:", common)
