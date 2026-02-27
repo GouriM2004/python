65abc227ae732267e83ee3c1e8bef51044b6b887
GouriM2004 <gourimore512@gmail.com>
2026-02-27 12:08:36 +0000

    trying to slove erroer

--- commonelement.py
+++ commonelement.py
@@ -1,3 +1,15 @@
+# Program to find common elements between two lists
+
+list1 = []
+list2 = []
+
+n1 = int(input("Enter number of elements in first list: "))
+for i in range(n1):
+    list1.append(int(input(f"Enter element {i+1}: ")))
+
+n2 = int(input("Enter number of elements in second list: "))
+for i in range(n2):
+    list2.append(int(input(f"Enter element {i+1}: ")))
 
 common = []
 
