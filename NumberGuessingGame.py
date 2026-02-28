91c156232d23511fbb842d2dbc79aa989a371b4f
Gouri <gourimore512@gmail.com>
2026-02-28 18:45:16 +0530

    some changes in this text file

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -11,10 +11,3 @@ while True:
         guess = int(input("Enter your guess: "))
         attempts += 1
 
-        if guess < secret_number:
-            print("Too Low ⬇️")
-        elif guess > secret_number:
-            print("Too High ⬆️")
-        else:
-            print(f"🎉 Correct! You guessed the number in {attempts} attempts.")
-            break
