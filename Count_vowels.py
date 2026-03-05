992f92fd1b55b13235decca88da5ccc9377b2de4
Gouri More <gourimore512@gmail.com>
2026-03-05 08:14:04 +0000

    program is done

--- Count_vowels.py
+++ Count_vowels.py
@@ -5,7 +5,12 @@ text = input("Enter a string: ")
 vowels = 0
 consonants = 0
 
-
+for ch in text:
+    if ch.isalpha():   # check only alphabets
+        if ch in 'aeiouAEIOU':
+            vowels += 1
+        else:
+            consonants += 1
 
 print("Number of vowels:", vowels)
 print("Number of consonants:", consonants)
